46c104bad07151b816f3659238dbc9b891c4af36
Mike Knoles <MKnoles78@gmail.com>
2021-11-28 16:54:09 -0500

    added initial code

--- src/Garage.java
+++ src/Garage.java
@@ -1,9 +1,43 @@
 
+//Exercise 6.8 Solution: Garage.java
+//Program calculates charges for parking
+import java.util.Scanner;
+
 public class Garage {
-
+	// begin calculating charges
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 
+		double totalReceipts = 0.0; // total fee collected for the day
+		double fee; // the charge for the current customer
+		double hours; // hours for the current customer
+
+		// read in the first customer's hours
+		System.out.print("Enter number of hours (-1 to quit): ");
+		hours = input.nextDouble();
+
+		while (hours >= 0.0) {
+			// calculate and print the charges
+			fee = calculateCharges(hours);
+			totalReceipts += fee;
+			System.out.printf("Current charges: $%.2f, Total receipts: $%.2f%n", fee, totalReceipts);
+
+			// read in the next customer's hours
+			System.out.print("Enter number of hours (-1 to quit):");
+			hours = input.nextDouble();
+		}
 	}
 
-}
+	// determines fee based on time
+	public static double calculateCharges(double hours) {
+		// apply minimum charge
+		double charge = 2.0;
+
+		// add extra fees as applicable
+
+		// apply maximum charge if needed
+
+		return charge;
+	}
+
+} // end class Garage
